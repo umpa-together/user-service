97473fa70c2902e76a604c103f6e8c53c23511c1
blackSooooo <blacksooooo@naver.com>
2023-01-07 17:17:23 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UMPA - User Service
+
+UMPA의 user와 관련된 도메인을 다루는 서비스
---
+++ src/test/kotlin/com/umpa/user/UserApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.umpa.user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
